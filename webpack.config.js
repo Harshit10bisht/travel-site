ec74145b89f2e9df50054f0843e7962f03158de2
Harshit <harshitbisht1210@gmail.com>
2020-04-03 18:23:43 +0530

    Completed lazy loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path = require('path');
 
 module.exports = {
-    entry: "./app/assets/scripts/App.js",
+    entry: {
+        App: "./app/assets/scripts/App.js",
+        Vendor: "./app/assets/scripts/Vendor.js"
+    },
     output: {
         path: path.resolve(__dirname, "C:/Users/user/Desktop/Projects/travel-site/app/temp/scripts"),
-        filename: "App.js"
+        filename: "[name].js"
     },
     module: {
         rules: [
